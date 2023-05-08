10e64f021401f4bd824543e79819ac8a7896e7cb
Rajesh khan <rajesh.k@iserveu.co.in>
2023-05-08 14:51:45 +0530

    added json syntax exception

--- lazier/src/main/java/com/velox/lazier/LazierMainActivity.kt
+++ lazier/src/main/java/com/velox/lazier/LazierMainActivity.kt
@@ -2,9 +2,7 @@ package com.velox.lazier
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class LazierMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
